3c672a6c270a5da42d8dbec3802ab93758988c93
Teja Laveti <tejalaveti23@gmail.com>
2025-07-09 22:05:14 +0530

    Data Transformation added

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import dill
+
+def save_object(file_path, obj):
+    dir_path = os.path.dirname(file_path)
+    os.makedirs(dir_path, exist_ok=True)
+
+    with open(file_path, 'wb') as file_obj:
+        dill.dump(obj, file_obj)
